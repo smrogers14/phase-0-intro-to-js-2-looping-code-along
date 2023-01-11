ec059cfbef4c8614115b08bbff46ed797450d5f4
Stacie Rogers <stacierogers92@icloud.com>
2023-01-11 09:10:38 -0600

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const arr1 = []
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        arr1.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+        debugger }
+        return arr1
+}
+
+
+
+
+
+function countDown() {
+    let number = 10
+    while(number >= 0){
+        console.log(number--)
+        debugger
+}
+}
+countDown(10)
+    
